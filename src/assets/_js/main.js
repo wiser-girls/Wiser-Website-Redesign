8012dcfd9d7a1229bab6744554034d0bd5a74b08
Jon Burr <yojonathanwburr@gmail.com>
2019-02-19 11:40:47 -0800

    add check for form div

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -3,21 +3,23 @@ import App from "./App";
 // Example of dynamically loading cognito contact form in footer
 var controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#example-contact-form",
-  duration: 200,
-  reverse: false
-})
-  .addTo(controller)
-  .on("enter", function(e) {
-    $.ajax({
-      url: "https://services.cognitoforms.com/s/bbN8iw1MJUqjPe6aHn-_rw",
-      dataType: "script",
-      cache: true,
-      success: function() {
-        // Callback
-        console.log("script ready");
-        Cognito.load("forms", { id: "25" });
-      }
+if ($("#example-contact-form").length) {
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#example-contact-form",
+    duration: 200,
+    reverse: false
+  })
+    .addTo(controller)
+    .on("enter", function(e) {
+      $.ajax({
+        url: "https://services.cognitoforms.com/s/bbN8iw1MJUqjPe6aHn-_rw",
+        dataType: "script",
+        cache: true,
+        success: function() {
+          // Callback
+          console.log("script ready");
+          Cognito.load("forms", { id: "25" });
+        }
+      });
     });
-  });
+}
