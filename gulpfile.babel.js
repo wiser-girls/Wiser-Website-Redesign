9e3dd7d56e1a818286dc7622510ad25638684de4
tombeck <t@ocupop.com>
2020-04-13 12:02:59 -0700

    Updating patch for Cloudcannon suite and first pass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,17 +3,11 @@ const suite = require('@cloudcannon/suite')
 
 const { scripts } = require('./tasks/scripts')
 
-gulp.task(scripts)
-
 // suite.dev(gulp, {
 //   tasks: [scripts]
 // })
 
-// suite.dev(gulp, {
-//   serve: {
-
-//   }
-// })
+suite.dev(gulp)
 
 // function hello(cb) {
 //   console.log('hello from gulp');
